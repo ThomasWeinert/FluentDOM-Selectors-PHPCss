6b9eab8c0cdbf8a79d7302d5ded041498c0607f8
Thomas Weinert <thomas@weinert.info>
2015-07-08 13:54:22 +0200

    Added subtitles and expected output to query selector example

--- examples/querySelector.php
+++ examples/querySelector.php
@@ -16,10 +16,23 @@ HTML;
 $document = new FluentDOM\Document();
 $document->loadHTML($html);
 
+echo 'Iterator over list items:', "\n";
 foreach ($document->querySelectorAll('li') as $li) {
   var_dump((string)$li);
 }
 
+echo 'Fetch second list item in an ul:', "\n";
 var_dump(
   $document->querySelector('ul li:nth-child(2)')->textContent
 );
+
+/*
+Expected Output:
+
+Iterator over list items:
+string(3) "one"
+string(3) "two"
+string(5) "three"
+Fetch second list item in an ul:
+string(3) "two"
+*/
